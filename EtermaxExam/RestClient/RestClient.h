a13ccc79f6bc57a4a9ae23d3e6abf6ee3833c61a
Julian Centurion <julian.centurion@mercadolibre.com>
2017-01-26 20:10:06 -0300

    Create RestClient

---
+++ EtermaxExam/RestClient/RestClient.h
@@ -0,0 +1,13 @@
+//
+//  RestClient.h
+//  EtermaxExam
+//
+//  Created by Julian Centurion on 26/1/17.
+//  Copyright © 2017 Julian Centurion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RestClient : NSObject
+
+@end
