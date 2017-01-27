9cf1d7a7d5b0fe4c2decc9bfd76ee5cf20cd76c0
Julian Centurion <julian.centurion@mercadolibre.com>
2017-01-27 09:20:19 -0300

    Create RedditPostModelHandler

---
+++ EtermaxExam/RedditPostModelHandler.h
@@ -0,0 +1,13 @@
+//
+//  RedditPostModelHandler.h
+//  EtermaxExam
+//
+//  Created by Julian Centurion on 27/1/17.
+//  Copyright © 2017 Julian Centurion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RedditPostModelHandler : NSObject
+
+@end
---
+++ EtermaxExam/RedditPostModelHandler.m
@@ -0,0 +1,13 @@
+//
+//  RedditPostModelHandler.m
+//  EtermaxExam
+//
+//  Created by Julian Centurion on 27/1/17.
+//  Copyright © 2017 Julian Centurion. All rights reserved.
+//
+
+#import "RedditPostModelHandler.h"
+
+@implementation RedditPostModelHandler
+
+@end
