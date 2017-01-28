a5ff39d39b803139906a8e3e2457b3777d47c9e2
Julian Centurion <julian.centurion@mercadolibre.com>
2017-01-28 15:01:57 -0300

    Create PersistDataHandler

---
+++ EtermaxExam/PersistDataHandler.h
@@ -0,0 +1,13 @@
+//
+//  PersistDataHandler.h
+//  EtermaxExam
+//
+//  Created by Julian Centurion on 28/1/17.
+//  Copyright © 2017 Julian Centurion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PersistDataHandler : NSObject
+
+@end
---
+++ EtermaxExam/PersistDataHandler.m
@@ -0,0 +1,13 @@
+//
+//  PersistDataHandler.m
+//  EtermaxExam
+//
+//  Created by Julian Centurion on 28/1/17.
+//  Copyright © 2017 Julian Centurion. All rights reserved.
+//
+
+#import "PersistDataHandler.h"
+
+@implementation PersistDataHandler
+
+@end
