75886ba95884b2c998784598de50b3b6da1a1d59
Julian Centurion <julian.centurion@mercadolibre.com>
2017-01-28 09:49:58 -0300

    Create RedditPostTableViewCell

---
+++ EtermaxExam/RedditPostTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  RedditPostTableViewCell.h
+//  EtermaxExam
+//
+//  Created by Julian Centurion on 27/1/17.
+//  Copyright © 2017 Julian Centurion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RedditPostTableViewCell : UITableViewCell
+
+@end
---
+++ EtermaxExam/RedditPostTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  RedditPostTableViewCell.m
+//  EtermaxExam
+//
+//  Created by Julian Centurion on 27/1/17.
+//  Copyright © 2017 Julian Centurion. All rights reserved.
+//
+
+#import "RedditPostTableViewCell.h"
+
+@implementation RedditPostTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
